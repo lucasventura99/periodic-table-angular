d630f8cbabc30a1acff9d89e78b2217e998a75db
Lucas <lucasventura99@hotmail.com>
2020-07-17 14:01:06 -0300

    Creating modules and components

---
+++ src/app/table/table.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PeriodicTableComponent } from './periodic-table/periodic-table.component';
+
+
+
+@NgModule({
+  declarations: [PeriodicTableComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TableModule { }
